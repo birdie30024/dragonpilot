63b54530a8641ce32900e1098a459d74d052e7d6
birdie30024 <112832468+birdie30024@users.noreply.github.com>
2022-09-14 07:25:41 -0400

    Update interface.py

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -57,8 +57,8 @@ class CarInterface(CarInterfaceBase):
       # Values from optimizer
       ret.steerRatio = 16.55  # 13.8 is spec end-to-end
       tire_stiffness_factor = 0.82
-      ret.lateralTuning.pid.kiBP, ret.lateralTuning.pid.kpBP = [[9., 22.], [9., 22.]]
-      ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.2, 0.35], [0.05, 0.09]]
+      ret.lateralTuning.pid.kiBP, ret.lateralTuning.pid.kpBP = [[4.47, 8.941, 20.12, 24.59, 29.06, 40.24], [4.47, 8.941, 20.12, 24.59, 29.06, 40.24]]
+      ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.01, 0.05, 0.1, 0.2, 0.32, 0.35], [0.0005, 0.001, 0.005, 0.05, 0.07, 0.09]]
     elif candidate in (CAR.SONATA, CAR.SONATA_HYBRID):
       ret.mass = 1513. + STD_CARGO_KG
       ret.wheelbase = 2.84
